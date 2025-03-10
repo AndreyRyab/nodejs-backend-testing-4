af9d83a7e6d7e8cf98ad5c5a65043c6bb6d60b89
Andrey Ryabtsev <ryab@yandex.ru>
2025-03-10 23:59:12 +0300

    added tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -20,13 +20,29 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany();
+
+      expect(result.length).toBe(posts.length);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany({skip: 1, limit: 2});
+
+      expect(result.length).toBe(2);
+      expect(result[0].text).toBe(posts[1].text);
+      expect(result[1].text).toBe(posts[2].text);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return empty array if skip is equal or less than posts.length', () => {
+      const result = postsService.findMany( { skip: posts.length });
+
+      expect(result.length).toBe(0);
+    });
+
+    it('should return empty array if limit is equal to 0', () => {
+      const result = postsService.findMany({ limit: 0 });
+
+      expect(result.length).toBe(0);
+    });
   });
 });
